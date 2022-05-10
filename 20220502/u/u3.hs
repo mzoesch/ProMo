d3da1814bb09a7bbc164e00fbb19132048359b73
mzoesch <magnus.zoeschinger@zoeschinger.de>
2022-05-10 17:02:05 +0200

    20220510

--- 20220502/u/u3.hs
+++ 20220502/u/u3.hs
@@ -13,6 +13,7 @@ a (x:xs)
     | True = a xs
 
 
+-- peer review ::
 -- b) Definieren Sie eine Funktion, die zu einer Liste (von Zahlen) alle ihrer geraden Zahlen und
 --    das Zweifache ihrer ungeraden Zahlen ausgibt. Beispiel: geradeZahlen2 [1,4,7,8] liefert die Liste [2,4,14,8]
 
@@ -20,21 +21,52 @@ a (x:xs)
 gerade :: Integral  a => a -> Bool
 gerade x = x `mod` 2 /= 1
 
-b :: [Integer]
-b = [x | x <- y]
+--b :: [Integer]
+--b = [x | x <- y]
 
 b' :: [Integer] -> [Integer]
 b' [] = []
-b'  (x:xs)
+b' (x:xs)
     | gerade x = x : b' xs
     | True = (x + x) : b' xs
 
+-- c) Definieren Sie eine Funktion, die für alle natürlichen Zahlen im Bereich zwischen zwei Zahlen (einschließlich dieser)
+--    eine Liste jener Zahlen zurückgibt, bei denen die Division durch 7 Rest 5 ergibt. Es darf vorausgesetzt werden,
+--    dass der erste Parameter immer kleiner als der zweite ist.
+-- 
+--    Beispiel: div7Rest5 7 28 ergibt [12,19,26]
 
+c :: Integer -> Integer -> [Integer]
+c x y = c' [x..y]
 
+c'' :: Integral  a => a -> Bool
+c'' x = x `mod` 7 == 5
 
+c' :: [Integer] -> [Integer]
+c' [] = []
+c' (x:xs)
+    | c'' x = x : c' xs
+    | otherwise = c' xs
 
+-- d) Definieren Sie eine Funktion, welche die Länge einer Liste berechnet,
+--    ohne die vordefinierte Funktion length zu benutzen.
 
+d :: [a] -> Int
+d [] = 0
+d (_:xs) = 1 + d xs
 
+-- e) Definieren Sie ein Funktion ’dreifach’, die einen Integer Wert als Eingabe erhält und
+--    dessen dreifachen Wert zurück gibt (siehe Übungsblatt 1 Aufgabe 2a).
+
+e :: Integer -> Integer
+e x = x + x + x
+
+-- f) Definieren Sie ein Funktion nurGrossBuchstaben :: [Char] -> [Char],
+--    die nur die Groß- buchstaben eines Strings ausgibt.
+--    
+--    Definieren Sie eine zweite Funktion, putStrLnNurGrossBuchstaben :: [Char] -> IO (), 
+--    die die Funktion nurGrossBuchstaben und deren Ergebnis auf die Konsole ausgibt.
+--    Benutzen Sie hierfür den “.” Operator.
 
 
 
